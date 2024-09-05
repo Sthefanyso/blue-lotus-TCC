64abebeefbe7b2fe523234673f7ebd26b8304da1
Sthefanyso <sthefanydsilva29@gmail.com>
2024-09-05 15:49:36 -0300

    telas login e about finalizadas

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private location: Location) {}
 
+  voltar() {
+    this.location.back();
+  }
 }
