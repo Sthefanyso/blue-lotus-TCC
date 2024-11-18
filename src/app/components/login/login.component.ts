36a98463b52a2bd90f89ac8a71c0cd51c28f51db
beamatos03 <beamatos03>
2024-11-18 00:37:22 -0300

    erro de email duplicado

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,12 +3,14 @@ import { RouterLink, Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { AuthService } from '../../auth.service';
 import { FormsModule } from '@angular/forms';
+import { ModalComponent } from "../modal/modal.component";
+
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, FormsModule,],
+  imports: [RouterLink, FormsModule, ModalComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -16,6 +18,9 @@ export class LoginComponent {
   email: string = '';
   password: string = '';
   message: string = '';
+    showModal: boolean = false;
+  title: string = 'Falha no login';  // Título da tela inicial
+  imageUrl = 'assets/recover-password/loading.svg';
 
   constructor(private location: Location, private authService: AuthService, private router: Router) {}
 
@@ -32,7 +37,20 @@ export class LoginComponent {
       },
       (error) => {
         console.error('Falha no login', error);
+        this.message = 'Ocorreu um erro desconhecido. Por favor, tente novamente.';
+        this.openModal();
+        if (error.status === 401) {
+           this.message = 'E-mail ou usuário incorreto. Tente novamente.';
+         } else if (error.status === 500) {
+           this.message = 'Erro interno do servidor. Por favor, tente novamente mais tarde.';
+         }
       }
     );
+    
   }
+
+
+  openModal() {
+    this.showModal = true;  // Fecha o modal
+}
 }
